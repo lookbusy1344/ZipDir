b05c4c0fd803fbf66243f9406174612d66c6106c
lookbusy1344 <3680611+lookbusy1344@users.noreply.github.com>
2025-07-31 20:12:26 +0100

    Publish notes for -r win-arm64

--- Publish.cmd
+++ Publish.cmd
@@ -4,5 +4,6 @@ dotnet clean --verbosity minimal
 echo Publishing native binary...
 
 rem dotnet publish ZipDir.csproj -c Release -r win-x64 --self-contained false /p:PublishSingleFile=true
+rem dotnet publish ZipDir.csproj -r win-arm64 -c Release
 dotnet publish ZipDir.csproj -r win-x64 -c Release
 pause
